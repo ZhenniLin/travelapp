3b503cca1266e0c0487a8b49fdbb4834c0b3b5b9
ZhenniLin <jeannelin2021@163.com>
2023-03-22 15:21:49 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
   <home-header></home-header>
+  <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 
 export default {
   name: 'Home',
   components: {
     // HomeHeader: HomeHeader
     // ES6里对象和键值都一样的话
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
