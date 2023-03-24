6df2bbc0f62aa710da4a9a901ff14236143ad7da
ZhenniLin <jeannelin2021@163.com>
2023-03-24 14:14:00 +0800

    city router

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
 
 Vue.use(Router)
 
 // 当用用户访问根路径时，展示给用户helloworld组件
 // 恰好App.vue里写了 router-view 展示的是由当前路由地址所配置的内容
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
+  }]
 })
